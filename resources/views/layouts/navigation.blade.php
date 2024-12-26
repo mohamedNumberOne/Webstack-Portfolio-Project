55d28220d5558cf3175eb24e0a616cec0d4efcc4
mohamed <mohamedmld954@gmail.com>
2024-12-26 22:15:53 +0100

    last commit 3

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,12 +13,14 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+
 
                     @auth
                         @if (Auth::user()->role == 'admin')
+                            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                {{ __('Dashboard') }}
+                            </x-nav-link>
+
                             <x-nav-link :href="route('add_product')" :active="request()->routeIs('add_product')">
                                 {{ __('Add Product') }}
                             </x-nav-link>
@@ -33,7 +35,7 @@
                         </x-nav-link>
                         <x-nav-link :href="route('show_messages')" :active="request()->routeIs('show_messages')">
                             {{ __('Messages') }}
-                        </x-nav-link>                        
+                        </x-nav-link>
                     @endauth
 
 
@@ -105,13 +107,14 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
+
         @auth
             @if (Auth::user()->role == 'admin')
+                <div class="pt-2 pb-3 space-y-1">
+                    <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-responsive-nav-link>
+                </div>
                 <div class="pt-2 pb-3 space-y-1">
                     <x-responsive-nav-link :href="route('add_product')" :active="request()->routeIs('add_product')">
                         {{ __('Add Product') }}
